3c7b5c53848bc9687be8dde7d9d1e31376090cf0
samar <semomoamer38@gmail.com>
2025-09-24 20:19:18 +0300

    feat: Add API and 404 error handling

--- server.js
+++ server.js
@@ -5,15 +5,28 @@ const path = require('path');
 const filePath = path.join(__dirname, 'index.html');
 
 const server = http.createServer((req, res) => {
-  fs.readFile(filePath, 'utf8', (err, data) => {
-    if (err) {
-      res.writeHead(500, { 'Content-Type': 'text/plain' });
-      res.end('Internal Server Error');
-      return;
-    }
-    res.writeHead(200, { 'Content-Type': 'text/html' });
-    res.end(data);
-  });
+    // Handle API request
+  if (req.url === '/api/ping') {
+    res.writeHead(200, { 'Content-Type': 'application/json' });
+    res.end(JSON.stringify({ message: 'pong' }));
+    return;
+  }
+    // Serve the HTML file for root requests
+  if (req.url === '/' || req.url === '/index.html') {
+    fs.readFile(filePath, 'utf8', (err, data) => {
+      if (err) {
+        res.writeHead(500, { 'Content-Type': 'text/plain' });
+        res.end('Internal Server Error');
+        return;
+      }
+      res.writeHead(200, { 'Content-Type': 'text/html' });
+      res.end(data);
+    });
+    return;
+  }
+
+  res.writeHead(404, { 'Content-Type': 'text/plain' });
+  res.end('404 Not Found');
 });
 
 server.listen(3000, () => {
